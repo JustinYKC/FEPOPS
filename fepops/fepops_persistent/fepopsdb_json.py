b9ae28c522eb71c6e998a2d6c1832724492832be
Steven Shave <stevenshave@gmail.com>
2023-05-15 13:39:08 +0100

    Removed magic values, and calc_sim from persistent

--- fepops/fepops_persistent/fepopsdb_json.py
+++ fepops/fepops_persistent/fepopsdb_json.py
@@ -1,101 +1,97 @@
-from .fepops_persistent_abc import FepopsPersistentAbstractBaseClass
-from scipy.spatial.distance import cdist
-from typing import Union
-from pathlib import Path
 import bz2
-from joblib import Parallel, delayed
-import numpy as np
 import json
-from base64 import b64encode, b64decode
+from base64 import b64decode, b64encode
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+
+from .fepops_persistent_abc import FepopsPersistentAbstractBaseClass
+
 
 class FepopsDBJSON(FepopsPersistentAbstractBaseClass):
-	def __init__(
-		self,
-		database_file: Union[str, Path],
-		kmeans_method: str = "pytorch-cpu",
-		parallel: bool = True,
-		n_jobs: int = -1,
-	):
-		super().__init__(
-			database_file = database_file,
-			kmeans_method = kmeans_method,
-			parallel = parallel,
-			n_jobs = n_jobs,
-		)
-		
-		if not self.database_file.parent.exists():
-			self.database_file.parent.mkdir(parents=True)
-		if self.database_file.exists():
-			self.db = json.load(open(self.database_file, "r"))
-		else:
-			self.db={}
-		self._db_changed = False
-		self._was_written = False
-			
-	def add_fepop(self, rdkit_canonical_smiles: str, fepops: Union[np.ndarray, None]):
-		if fepops is None: fepops = np.array([np.NaN])
-		super().add_fepop(rdkit_canonical_smiles=rdkit_canonical_smiles, fepops=fepops)
-		if not self.fepop_exists(rdkit_canonical_smiles=rdkit_canonical_smiles):
-			self.db[rdkit_canonical_smiles] = b64encode(bz2.compress(fepops.tobytes())).decode('ascii')
-			self._db_changed = True
-			
-	def get_fepop(self, smiles: str, is_canonical:bool=False) -> Union[np.ndarray, None]:
-		super().get_fepop(smiles=smiles)
-		if not is_canonical:
-			smiles, mol = self._get_can_smi_mol_tuple(smiles)
-		if self.fepop_exists(rdkit_canonical_smiles=smiles):
-			res = np.frombuffer(bz2.decompress(b64decode(self.db[smiles].encode())))
-			if np.isnan(res).any(): return None
-			else: return res.reshape(7,-1)
-		else:
-			new_fepops = self.fepops_object.get_fepops(mol)
-			self.add_fepop(rdkit_canonical_smiles=smiles, fepops=new_fepops)
-			return new_fepops
-		
-	def fepop_exists(self, rdkit_canonical_smiles: str) -> bool:
-		"""Check if Fepop exists in the database
+    def __init__(
+        self,
+        database_file: Union[str, Path],
+        kmeans_method: str = "pytorch-cpu",
+        parallel: bool = True,
+        n_jobs: int = -1,
+    ):
+        super().__init__(
+            database_file=database_file,
+            kmeans_method=kmeans_method,
+            parallel=parallel,
+            n_jobs=n_jobs,
+        )
 
-		If the fepops object was constructed with a database file, then
-		query if the supplied canonical SMILES is included.  If no database
-		is present, then False is returned, as if it is not included.
+        if not self.database_file.parent.exists():
+            self.database_file.parent.mkdir(parents=True)
+        if self.database_file.exists():
+            self.db = json.load(open(self.database_file, "r"))
+        else:
+            self.db = {}
+        self._db_changed = False
+        self._was_written = False
 
-		Parameters
-		----------
-		rdkit_canonical_smiles : str
-			Canonical smiles to check
+    def add_fepop(self, rdkit_canonical_smiles: str, fepops: Union[np.ndarray, None]):
+        if fepops is None:
+            fepops = np.array([np.NaN])
+        super().add_fepop(rdkit_canonical_smiles=rdkit_canonical_smiles, fepops=fepops)
+        if not self.fepop_exists(rdkit_canonical_smiles=rdkit_canonical_smiles):
+            self.db[rdkit_canonical_smiles] = b64encode(
+                bz2.compress(fepops.tobytes())
+            ).decode("ascii")
+            self._db_changed = True
 
-		Returns
-		-------
-		bool
-			True if the canonical smiles exists in the database
-		"""
-		return rdkit_canonical_smiles in self.db
-	
-	def calc_similarity(self, 
-		query: Union[np.ndarray, str],
-		candidate: Union[np.ndarray, str],
-	) -> float:
-		if isinstance(query, str):
-			query = self.get_fepop(query)
-		if isinstance(candidate, str):
-			candidate = self.get_fepop(candidate)
-		super().calc_similarity(fepops_features_1=query, fepops_features_2=candidate)
-		return np.max(cdist(query, candidate, metric=self.fepops_object._score_combialign))
-	
-	def write(self):
-		if self._db_changed:
-			json.dump(self.db, open(self.database_file, "w"))
-			self._was_written = True
+    def get_fepop(
+        self, smiles: str, is_canonical: bool = False
+    ) -> Union[np.ndarray, None]:
+        super().get_fepop(smiles=smiles)
+        if not is_canonical:
+            smiles, mol = self._get_can_smi_mol_tuple(smiles)
+        if self.fepop_exists(rdkit_canonical_smiles=smiles):
+            res = np.frombuffer(bz2.decompress(b64decode(self.db[smiles].encode())))
+            if np.isnan(res).any():
+                return None
+            else:
+                return res.reshape(self.fepops_object.num_fepops_per_mol, -1)
+        else:
+            new_fepops = self.fepops_object.get_fepops(mol)
+            self.add_fepop(rdkit_canonical_smiles=smiles, fepops=new_fepops)
+            return new_fepops
 
-	def __enter__(self):
-		return self
-	def __exit__(self, exc_type, exc_value, traceback):
-		self.write()
+    def fepop_exists(self, rdkit_canonical_smiles: str) -> bool:
+        """Check if Fepop exists in the database
 
-	def __del__(self):
-		if self._db_changed and not self._was_written:
-			print("New fepops were added but changes were not written. Either call .write() or use FepopsDBJSON in a context, like with FepopsDBJSON() as fepops_jsondb...")
-			
+        If the fepops object was constructed with a database file, then
+        query if the supplied canonical SMILES is included.  If no database
+        is present, then False is returned, as if it is not included.
 
+        Parameters
+        ----------
+        rdkit_canonical_smiles : str
+                Canonical smiles to check
 
-	
+        Returns
+        -------
+        bool
+                True if the canonical smiles exists in the database
+        """
+        return rdkit_canonical_smiles in self.db
+
+    def write(self):
+        if self._db_changed:
+            json.dump(self.db, open(self.database_file, "w"))
+            self._was_written = True
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.write()
+
+    def __del__(self):
+        if self._db_changed and not self._was_written:
+            print(
+                "New fepops were added but changes were not written. Either call .write() or use FepopsDBJSON in a context, like with FepopsDBJSON() as fepops_jsondb..."
+            )
