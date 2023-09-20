873f200d4bf680d65850c62175d41638f498f064
Steven Shave <stevenshave@gmail.com>
2023-09-20 21:37:46 +0100

    Added kmeans_method arg to storgeobject getter #18

--- src/fepops/fepops_persistent/utils.py
+++ src/fepops/fepops_persistent/utils.py
@@ -5,12 +5,17 @@ from pathlib import Path
 
 def get_persistent_fepops_storage_object(
     database_file: Union[str, Path],
+    kmeans_method: str = "sklearn",
     parallel: bool = True,
     n_jobs: int = -1,
 ) -> Union[FepopsDBSqlite, FepopsDBJSON]:
     if str(database_file).endswith(
         (".sqlite", ".sqlite3", ".db", ".db3", ".s3db", ".sl3")
     ):
-        return FepopsDBSqlite(database_file, parallel=parallel, n_jobs=n_jobs)
+        return FepopsDBSqlite(
+            database_file, kmeans_method=kmeans_method, parallel=parallel, n_jobs=n_jobs
+        )
     if str(database_file).endswith((".json")):
-        return FepopsDBJSON(database_file, parallel=parallel, n_jobs=n_jobs)
+        return FepopsDBJSON(
+            database_file, kmeans_method=kmeans_method, parallel=parallel, n_jobs=n_jobs
+        )
